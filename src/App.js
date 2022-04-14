11037b5510884ff122a39adf831d358669f9d01c
Luis Dominguez <ladominguez@assanet.com>
2022-04-13 23:15:35 -0500

    test de barraza y cia

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+
+import React from 'react'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Test de Barraza y CIA</h1>
       </header>
+
+      <p>Empresa panameña Barraza y CIA, S.A.. Nuestras marcas acompañan a miles de hogares panameños todos los días.
+      Son parte de nuestra cultura; son parte de las tradiciones que pasan de generación en generación.</p>
+      <p>
+      Son parte de nuestra cultura; son parte de las tradiciones que pasan de generación en generación.</p>
     </div>
+    
   );
 }
 
